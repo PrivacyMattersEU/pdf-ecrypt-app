8b266d88c405ecc23fa5e3a246dcd9cd89fa59d3
Thomas Strohmeier <office@thomas-strohmeier.com>
2019-10-12 18:54:58 +0200

    fix warning

--- src/ProcessingFiles/index.js
+++ src/ProcessingFiles/index.js
@@ -78,7 +78,7 @@ function password_generator( len ) {
         let entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
         let entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
         let hold = string.charAt( entity1 );
-        hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
+        hold = (password.length % 2 === 0)?(hold.toUpperCase()):(hold);
         character += hold;
         character += numeric.charAt( entity2 );
         character += punctuation.charAt( entity3 );
